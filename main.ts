53a32c5dc68b24b9a6aae5bf6176aaeaf69037e5
Screeechkid <113040110+Screeechkid@users.noreply.github.com>
2022-09-07 10:41:09 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,25 @@
+input.onButtonPressed(Button.A, function () {
+    music.ringTone(330)
+    music.ringTone(294)
+    music.ringTone(262)
+    music.ringTone(294)
+    music.ringTone(330)
+    music.ringTone(330)
+    music.ringTone(330)
+    music.ringTone(330)
+    music.ringTone(294)
+    music.ringTone(294)
+    music.ringTone(330)
+    music.ringTone(294)
+    music.ringTone(262)
+})
+basic.showLeds(`
+    . . . # .
+    . . # . #
+    . . # . .
+    # # # . .
+    # # . . .
+    `)
+basic.forever(function () {
+	
+})
